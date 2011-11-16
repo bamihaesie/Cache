b50e80aae045ed0e7d7e85fb4fa5548d0efaaf89
Bogdan Amihaesie <bogdan.am6@gmail.com>
2011-11-16 00:06:35 +0000

    lru implementation

--- src/main/java/com/bogdan/cache/Cache.java
+++ src/main/java/com/bogdan/cache/Cache.java
@@ -1,5 +1,20 @@
 package com.bogdan.cache;
 
-public class Cache {
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Cache<K, V> extends LinkedHashMap<K, V> {
+
+    private final int maxEntries;
+
+    public Cache(int maxEntries) {
+        super(maxEntries + 1, 1.0f, true);
+        this.maxEntries = maxEntries;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(final Map.Entry<K, V> eldest) {
+        return super.size() > maxEntries;
+    }
 
 }
--- src/test/java/com/bogdan/cache/CacheTest.java
+++ src/test/java/com/bogdan/cache/CacheTest.java
@@ -1,17 +1,47 @@
 package com.bogdan.cache;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 /**
  * Unit test for Cache.
  */
 public class CacheTest {
 
+    private Cache<String, String> cache;
+
+    @Before
+    public void setUp() {
+        cache = new Cache<String, String>(1);
+    }
+
     @Test
-    public void firstTest() {
-        assertTrue(true);
+    public void putAndGetTest() {
+        cache.put("test", "value");
+        String value = cache.get("test");
+        assertNotNull("Should get not null value back.", value);
+        assertEquals("value", value);
+    }
+
+    @Test
+    public void leastRecentlyUsedTest() {
+        cache = new Cache<String, String>(2);
+        assertEquals(0, cache.keySet().size());
+        cache.put("key1", "value1");
+        assertEquals(1, cache.keySet().size());
+        cache.put("key2", "value2");
+        assertEquals(2, cache.keySet().size());
+        cache.get("key1");
+        cache.get("key2");
+        cache.put("key3", "value3");
+        assertEquals(2, cache.keySet().size());
+        assertNull(cache.get("key1"));
+        assertNotNull(cache.get("key2"));
+        assertNotNull(cache.get("key3"));
     }
 
 }
